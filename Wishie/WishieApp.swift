4a70dc4aeeed0964cde569b2f63a69644fb22c26
LilKhang1810 <95366428+LilKhang1810@users.noreply.github.com>
2025-10-05 10:45:50 +0700

    Initial Commit

---
+++ Wishie/WishieApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishieApp.swift
+//  Wishie
+//
+//  Created by Nguyễn Khang Hữu on 5/10/25.
+//
+
+import SwiftUI
+
+@main
+struct WishieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WishieTests/WishieTests.swift
@@ -0,0 +1,16 @@
+//
+//  WishieTests.swift
+//  WishieTests
+//
+//  Created by Nguyễn Khang Hữu on 5/10/25.
+//
+
+import Testing
+
+struct WishieTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

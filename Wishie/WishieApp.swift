e3142b75aaed78953a9997c07ff73563c179ddaf
Khang Huu Nguyen <khanghnguyen@kms-technology.com>
2026-05-20 06:39:38 +0700

    fix crash

--- Wishie/WishieApp.swift
+++ Wishie/WishieApp.swift
@@ -9,28 +9,23 @@ import SwiftUI
 import Combine
 import Firebase
 
-class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        FirebaseApp.configure()
-        
-        return true
-    }
-}
-
 @main
 struct WishieApp: App {
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     @StateObject private var authViewModel: AuthViewModel
     @StateObject private var coordinator: RootNavigationCoordinator
     @State private var isActive: Bool = false
-
+    
     init() {
+        if FirebaseApp.app() == nil {
+            
+            FirebaseApp.configure()
+            
+        }
         let auth = AuthViewModel()
         _authViewModel = StateObject(wrappedValue: auth)
         _coordinator = StateObject(wrappedValue: RootNavigationCoordinator(authViewModel: auth))
     }
-
+    
     var body: some Scene {
         WindowGroup {
             ZStack {

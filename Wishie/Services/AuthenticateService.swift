00a45193e3c704eef4d82dc135cdb24da74cc9b8
Khang Huu Nguyen <khanghnguyen@kms-technology.com>
2026-07-07 17:07:12 +0700

    feat: add signInWithApple to AuthenticateService

--- Wishie/Services/AuthenticateService.swift
+++ Wishie/Services/AuthenticateService.swift
@@ -14,6 +14,7 @@ import Combine
 protocol AuthenticateServiceProtocol {
     func login(_ email: String, _ password: String) -> AnyPublisher<AuthDataResult?, Error>
     func signUp(_ signUpRequest: SignUpRequest) -> AnyPublisher<FirebaseAuth.AuthDataResult?, Error>
+    func signInWithApple(idToken: String, rawNonce: String, fullName: PersonNameComponents?) -> AnyPublisher<AuthDataResult?, Error>
     func resetPassword(_ email: String) -> AnyPublisher<Bool, Error>
     func getUserInfo() async throws -> UserModel?
     func getUserInfo(by userId: String) async throws -> UserModel?
@@ -66,6 +67,44 @@ class AuthenticateService: AuthenticateServiceProtocol {
                     } else {
                         promise(.success(result))
                     }
+                }
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+    func signInWithApple(idToken: String, rawNonce: String, fullName: PersonNameComponents?) -> AnyPublisher<AuthDataResult?, Error> {
+        return Future<AuthDataResult?, Error> { [weak self] promise in
+            guard let self else { return }
+            let credential = OAuthProvider.appleCredential(withIDToken: idToken, rawNonce: rawNonce, fullName: fullName)
+            self.auth.signIn(with: credential) { result, error in
+                if let error {
+                    promise(.failure(error))
+                    return
+                }
+                guard let result else {
+                    promise(.failure(NSError(domain: "SignInWithAppleError", code: -1, userInfo: [NSLocalizedDescriptionKey: "Authentication result is nil."])))
+                    return
+                }
+                guard result.additionalUserInfo?.isNewUser == true else {
+                    promise(.success(result))
+                    return
+                }
+                let user = result.user
+                let userData: [String: Any] = [
+                    "uid": user.uid,
+                    "firstName": fullName?.givenName ?? "",
+                    "lastName": fullName?.familyName ?? "",
+                    "email": user.email ?? "",
+                    "phone": "",
+                    "dateOfBirth": Timestamp(date: Date()),
+                    "createAt": FieldValue.serverTimestamp()
+                ]
+                self.db.collection("users").document(user.uid).setData(userData) { error in
+                    if let error {
+                        promise(.failure(error))
+                        return
+                    }
+                    promise(.success(result))
                 }
             }
         }
